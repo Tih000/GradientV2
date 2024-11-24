fca78555fea3a62853d14a7b711afd93dd0cd75a
Tih000 <tbludov2004@gmail.com>
2024-11-24 18:35:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 HEADLESS = True
-shuffle = False
+shuffle = False                     # Перемешивать почты или идти по порядку (True or False)
 
-delay_min = 40
-delay_max = 60
+delay_min = 40                      # Задержка между аккаунтами при работе
+delay_max = 60                      # (для корректной работы не советуем делать маленькую задержку)
 
-delay_for_getting_stats_min = 40
-delay_for_getting_stats_max = 60
+delay_for_getting_stats_min = 30    # Задержка между аккаунтами при режиме stats
+delay_for_getting_stats_max = 40    # (для корректной работы не советуем делать маленькую задержку)
 
-DELAY_BETWEEN_GETTING_STATS = 500
+DELAY_BETWEEN_GETTING_STATS = 500   # Среднее значеие (+-100) при проверки статистики в режиме farming
 
-DINAMIC_PROXY = True
+DINAMIC_PROXY = False                # Используются ли динамические прокси (True or False)
+
+TELEGRAM = False                    # Используется ли Телеграм  (True or False)
+TELEGRAM_STATS_DELAY = 3600         # Задержка между выводом статистики в Телеграмм
+BOT_TOKEN = ''                      # Токен Телеграм-Бота
+CHAT_ID = ''                        # Ваш chatid (@getmyid_bot)
